b07e4c040d9c392511dfeb772ecf4f76a2fbecd8
Scott Huchton <shuchton@gmail.com>
2023-08-29 13:19:39 -0400

    added button plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -23,5 +23,7 @@ module.exports = {
   plugins: [
     require("./plugins/openVariant"),
     require("./plugins/animationDelay"),
+    require("./plugins/tableCaption"),
+    require("./plugins/buttonPlugin")
   ],
 };
